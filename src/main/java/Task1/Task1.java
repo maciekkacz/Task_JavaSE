7f197d45d0bf195ee96321342e4446971d8b1325
Maciek Kacz <maciek.kacz@wp.pl>
2021-02-01 16:05:40 +0100

    Task_one add 3 numbers and return sum

---
+++ src/main/java/Task1/Task1.java
@@ -0,0 +1,20 @@
+package Task1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double number1 = 2.0;
+        double number2 = 5.0;
+        double number3 = 10.0;
+        double sum = addAllNumbers2( number1, number2, number3 );
+        System.out.println(sum);
+
+    }
+
+    private static double addAllNumbers2(double a, double b, double c) {
+        double sum;
+        sum = a + b + c;
+        return sum;
+    }
+
+
+}
